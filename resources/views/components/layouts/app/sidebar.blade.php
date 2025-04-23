68d78cea8c3f519414f220d85648999b2cd0209a
jamalapriadi <jamal.apriadi@gmail.com>
2025-04-23 09:26:21 +0700

    Feature-Add Modul Product Categories

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -12,10 +12,15 @@
             </a>
 
             <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')" class="grid">
+                <flux:navlist.group :heading="__('Menu')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+
+                    <flux:navlist.item icon="clipboard-document-list" :href="route('categories.index')" :current="request()->routeIs('categories.index')" wire:navigate>{{ __('Categories') }}</flux:navlist.item>
+
+                    <flux:navlist.item icon="list-bullet" :href="route('products')" :current="request()->routeIs('products')" wire:navigate>{{ __('Products') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
+            
 
             <flux:spacer />
 
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
 
 
 
 //kode baru diubah menjadi seperti ini
-Route::get('/', [HomepageController::class, 'index']);
+Route::get('/', [HomepageController::class, 'index'])->name('home');
 Route::get('products', [HomepageController::class, 'products']);
 Route::get('product/{slug}', [HomepageController::class, 'product']);
 Route::get('categories',[HomepageController::class, 'categories']);
@@ -16,10 +18,14 @@ Route::get('cart', [HomepageController::class, 'cart']);
 Route::get('checkout', [HomepageController::class, 'checkout']);
 
 
+Route::group(['prefix'=>'dashboard'], function(){
+    Route::get('/',[DashboardController::class,'index'])->name('dashboard');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    Route::resource('categories',ProductCategoryController::class);
+    Route::get('products',[DashboardController::class,'products'])->name('products');
+
+})->middleware(['auth', 'verified']);
+
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
